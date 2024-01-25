fe84992a52f5acbdfbe7ca1ad4f64633f7981747
gergovari <work@varigergo.hu>
2024-01-25 11:00:19 +0100

    fix

--- lazycontent/feedreaper/storage_config/__main__.py
+++ lazycontent/feedreaper/storage_config/__main__.py
@@ -2,7 +2,7 @@ from .. import StorageConfig
 
 main_callable = StorageConfig
 
-def _StorageConfig(feed_file: {"widget_name": "text_preview", "type": str} = "feeds.opml", used_file: {"widget_name": "text_preview", "type": str} = "used.txt") -> [{"name": "storage_config", "type": StorageConfig}]:
+def _StorageConfig(feeds_file: {"widget_name": "text_preview", "type": str} = "feeds.opml", used_file: {"widget_name": "text_preview", "type": str} = "used.txt") -> [{"name": "storage_config", "type": StorageConfig}]:
 	pass
 
 signature_callable = _StorageConfig
--- lazycontent/lazysummary/summarizer_node/__main__.py
+++ lazycontent/lazysummary/summarizer_node/__main__.py
@@ -6,6 +6,6 @@ from ...lazycommon.entry import Entry
 from typing import Type, List
 
 def summarize(summarizer: Summarizer, type: Type[Content], entries: List[Entry]) -> [{ "name": "summarized_blocks" , "type": List[str] }]:
-	return summarizer.summarize(entries, typ)
+	return summarizer.summarize(entries, type)
 
 main_callable = summarize
