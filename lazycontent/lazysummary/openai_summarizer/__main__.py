af5c760094e170f7f6f3b7dc3d982ec7a65338cb
gergovari <work@varigergo.hu>
2024-01-25 09:13:41 +0100

    feat: openai summarizer node

---
+++ lazycontent/lazysummary/openai_summarizer/__main__.py
@@ -0,0 +1,8 @@
+from ..openai import OpenAIChat, OpenAISummarizer
+
+main_callable = OpenAISummarizer
+
+def _OpenAISummarizer(api: OpenAIChat) -> [{"name": "summarizer", "type": OpenAISummarizer}]:
+	pass
+
+signature_callable = _OpenAISummarizer
